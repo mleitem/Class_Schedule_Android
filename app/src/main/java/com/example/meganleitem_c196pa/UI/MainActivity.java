968a755cfc895439d77dcddf8ea97fc2c514999a
mleitem <megan.leitem@yahoo.com>
2022-07-07 13:02:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/meganleitem_c196pa/UI/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.meganleitem_c196pa.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.meganleitem_c196pa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
