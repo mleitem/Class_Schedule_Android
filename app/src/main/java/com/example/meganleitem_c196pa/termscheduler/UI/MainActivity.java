396ce8431a483672804796922468834a36b2013d
mleitem <megan.leitem@yahoo.com>
2022-09-15 12:30:43 -0700

    Added login functionality to login page

--- app/src/main/java/com/example/meganleitem_c196pa/termscheduler/UI/MainActivity.java
+++ app/src/main/java/com/example/meganleitem_c196pa/termscheduler/UI/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.meganleitem_c196pa.R;
 import com.example.meganleitem_c196pa.termscheduler.Database.Repository;
@@ -13,30 +15,57 @@ import com.example.meganleitem_c196pa.termscheduler.Entity.Course;
 import com.example.meganleitem_c196pa.termscheduler.Entity.Term;
 import com.example.meganleitem_c196pa.termscheduler.Entity.User;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static int numAlert;
+
+    EditText userUsername;
+    EditText userPassword;
+
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User user1 = new User(1, "test", "test");
+        User user2 = new User(2, "admin", "admin");
+        Repository repo = new Repository(getApplication());
+        repo.insert(user1);
+        repo.insert(user2);
+    }
+
+    public boolean loginCheck(String username, String password){
+        Repository repo = new Repository(getApplication());
+        List<User> allUsers = repo.getAllUsers();
+        Boolean login = false;
+
+        for(int i = 0; i < allUsers.size(); i++) {
+            if (username.equals(allUsers.get(i).getUsername()) && password.equals(allUsers.get(i).getPassword())) {
+                login = true;
+            }
+        }
+        return login;
     }
 
     public void enterHere(View view) {
+        userUsername = findViewById(R.id.username);
+        userPassword = findViewById(R.id.password);
+        username = userUsername.getText().toString();
+        password = userPassword.getText().toString();
 
-        Intent intent = new Intent(MainActivity.this, TermList.class);
-        startActivity(intent);
-        Repository repo = new Repository(getApplication());
-        User user1 = new User(1, "test", "test");
-        User user2 = new User(2, "admin", "admin");
-        repo.insert(user1);
-        repo.insert(user2);
-        /*Term term = new Term(2, "Fall 2022", "10/1/2022", "12/1/2022");
-        Course course = new Course(1, "Anatomy", "6/1/2022", "9/1/2022", "In Progress", "Name", "Email", "800-400-3000", 1);
-        Assessment assessment = new Assessment(1, "Objective Assessment", "Anatomy Assessment", "08/10/2022", "08/11/2022", 1);
-        repo.insert(term);
-        repo.insert(course);
-        repo.insert(assessment);*/
+        if(loginCheck(username, password) == true){
+            Intent intent = new Intent(MainActivity.this, TermList.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Incorrect username and/or password. Please try again.", Toast.LENGTH_LONG).show();
+        }
+
+
+
     }
 }
